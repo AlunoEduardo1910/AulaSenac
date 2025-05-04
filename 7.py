7bdec4b901503d0e7a60856debc3ffa2d1f0dcf0
Eduardo.Goncalves <goncalvespereira02edu@gmail.com>
2025-05-03 21:12:09 -0300

    Exercício 7

--- 7.py
+++ 7.py
@@ -2,4 +2,4 @@ par_ou_impar = int(input("Digite um número para verificar se é impar ou par: "
 
 resultado = par_ou_impar % 2 == 0
 
-print(f"Número: {par_ou_impar}, resultado: {resultado}")
+print(f"Número: {par_ou_impar}, é par? {resultado}")
