b3ee9545b9caf0a3e3a4cbd9e141a88a13d795d9
Eduardo.Goncalves <goncalvespereira02edu@gmail.com>
2025-05-03 21:35:31 -0300

    Exercício 9

---
+++ 9.py
@@ -0,0 +1,7 @@
+frutas = ["maçã", "pêssego", "morango", "melancia", "uva"]
+print(frutas)
+
+print("Removendo pêssego e adicionando banana...")
+frutas.append("banana")
+frutas.remove("pêssego")
+print(frutas)
